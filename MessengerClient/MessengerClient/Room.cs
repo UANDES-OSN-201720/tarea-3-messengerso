32eac7ec702675580eec372cef5c9fed9aa78047
Rabinovich1825 <baperez@miuandes.cl>
2017-11-21 19:17:16 -0300

    Files added

---
+++ MessengerClient/MessengerClient/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerSO
+{
+    class Room
+    {
+        List<Contact> members;
+        int id;
+
+        public Room(List<Contact> members, int id)
+        {
+            this.members = members;
+            this.id = id;
+        }
+
+        public void Send_message(string message)
+        {
+            foreach(Contact member in members)
+            {
+                member.Send_message(message, 'g', id);
+            }
+        }
+    }
+}
---
+++ MessengerServer/MessengerServer/MessengerServer/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MessengerServer
+{
+    class Client
+    {
+        IPAddress ip_address;
+
+        public Client(IPAddress ip_address)
+        {
+            this.ip_address = ip_address;
+        }
+    }
+}
