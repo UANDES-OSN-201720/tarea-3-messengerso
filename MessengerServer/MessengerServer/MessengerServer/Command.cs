be91a39e24690b456ef603fb635856aea04641f0
joacoferna <31423509+joacoferna@users.noreply.github.com>
2017-11-24 19:19:07 -0300

    Modified gitignore

---
+++ MessengerServer/MessengerServer/MessengerServer/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerServer
+{
+    class Command
+    {
+        private CommandType type; //Type of command to send
+        private IPAddress senderIP;
+        private string senderName;
+        private IPAddress target;
+        private string body;
+
+        public Command(CommandType type, IPAddress target, string metaData)
+        {
+            this.type = type;
+            this.target = target;
+            this.body = metaData;
+
+        }
+    }
+}
