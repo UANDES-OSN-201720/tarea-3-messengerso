ec27da0bc308d79d93b8774f356e2ef7f0e450e9
joacoferna <31423509+joacoferna@users.noreply.github.com>
2017-11-25 00:16:58 -0300

    Corrected error: client changes were not made in last commit

--- MessengerClient/MessengerClient/Program.cs
+++ MessengerClient/MessengerClient/Program.cs
@@ -5,16 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace MessengerClient
 {
     class Program
     {
-        private static Socket client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
+        private static TcpClient tcpClient = new TcpClient();
+        private static NetworkStream ns = tcpClient.GetStream();
+        private static StreamReader reader = new StreamReader(ns);
+        private static string username = reader.ReadLine();
         static void Main(string[] args)
         {
             LoopConnect();
+            Console.WriteLine("List of commands, and what you can do with them:");
+            Console.WriteLine("/setname : sets your username \n/tg : talk to group\n/" +
+                "t [user] : Talk to specific user, if it exists" + "/exit : exit current chat");
             SendLoop();
             Console.ReadLine();
         }
@@ -23,28 +29,24 @@ namespace MessengerClient
         {
             while (true)
             {
-                Console.Write("Enter a request: ");
-                string request = Console.ReadLine();
-                byte[] buffer = Encoding.UTF8.GetBytes(request);
-                client_socket.Send(buffer);
-
-                byte[] received_buff = new byte[1024];
-                int received = client_socket.Receive(received_buff);
-                byte[] data = new byte[received];
-                Array.Copy(received_buff, data, received);
-                Console.WriteLine("Received: " + Encoding.UTF8.GetString(data));
+                Console.Write("[" + username + "]: ");
+                StreamWriter writer = new StreamWriter(ns);
+                string command = Console.ReadLine();
+                writer.WriteLine(command);
+                writer.Flush();
+                Console.WriteLine(reader.ReadLine());
             }
         }
 
         private static void LoopConnect()
         {
             int attempts = 0;
-            while (! client_socket.Connected)
+            while (! tcpClient.Connected)
             {
                 try
                 {
                     attempts++;
-                    client_socket.Connect(IPAddress.Loopback, 8000);
+                    tcpClient.Connect("192.168.0.178", 1260);
                 }
                 catch (SocketException)
                 {
